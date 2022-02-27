dad5f7bafcd46856111b8a8e3ef16237bf3c6fa2
amcclead7336 <n7q4c1@u.northwestern.edu>
2022-02-27 17:32:20 -0600

    Added color to buttons

--- hackpad.py
+++ hackpad.py
@@ -8,8 +8,8 @@ import pyperclip3
 button_columns = 4
 
 def fill_output(parms, ctext, lname, des):
-		"""Clears the existing text from output and inserts the copy text with filled in
-		Parameters if they're available."""
+    """Clears the existing text from output and inserts the copy text with filled in
+    Parameters if they're available."""
 
     output.delete(0, END)
     for parm in parms:
@@ -26,7 +26,7 @@ def fill_output(parms, ctext, lname, des):
     
 
 def copy_button():
-		"""Copies text in output to user's clipboard"""
+    """Copies text in output to user's clipboard"""
 	
     pyperclip3.copy(output.get())
 
@@ -34,32 +34,32 @@ def copy_button():
 def main():
     with open("config.json", "r") as f:
         configs = json.loads(f.read())
-		
-		# Defining root Tk object that all other widgets will be stored in
+
+    # Defining root Tk object that all other widgets will be stored in
     root = Tk()
     root.title("Hackpad")
     # root.geometry("1025x725")
     # root.iconbitmap("images/winged-arrow_38729.ico")
 
-		# Breaking the main screen into sections
-		# Parameters frame
+    # Breaking the main screen into sections
+    # Parameters frame
     parms_frame = LabelFrame(root, text="Parameters", padx=5, pady=5)
     parms_frame.grid(row=0, column=0, padx=10, pady=10)
 
-		# Buttons notebook, enables different tabs for each section
+    # Buttons notebook, enables different tabs for each section
     button_notebook = ttk.Notebook(root)
     button_notebook.grid(row=1, column=0)
 
-		# Output Frame
+    # Output Frame
     output_frame = LabelFrame(root, text="Output", padx=5, pady=5)
     output_frame.grid(row=2, column=0, padx=10, pady=10)
 
-		# Description Frame
+    # Description Frame
     des_frame = LabelFrame(root, text="Description", padx=5, pady=5)
     des_frame.grid(row=0, column=1, padx=10, pady=10)
 
 
-		# Inside the button_notebook are LabelFrames for each section in the config.json
+    # Inside the button_notebook are LabelFrames for each section in the config.json
     sections = []
     # Filling parms for all buttons
     parms = []
@@ -70,24 +70,31 @@ def main():
         
         button_row = 0
         button_count = 0
-        
+
+        button_h= 3
+        button_w= 10
+
         for key in section["keys"]:
             fill_output2 = partial(fill_output, key["parms"], key["ctext"], key["lname"], key["des"])
-            
+
             try:
-            		buttons.append(Button(sections[-1], text=key["qname"], bg=key["color"], width=15, height=5, command=fill_output2))
+                buttons.append(Button(sections[-1], text=key["qname"], highlightbackground=key["color"], width=button_w, height=button_h, command=fill_output2))
+                print("Color Added")
             
             except KeyError:
-            		buttons.append(Button(sections[-1], text=key["qname"], width=15, height=5, command=fill_output2))
-            		
+                print("No Color")
+                buttons.append(Button(sections[-1], text=key["qname"], width=button_w, height=button_h, command=fill_output2))
+
             parms += key["parms"]
-						
-						if button_count != 0 and button_count % button_columns == 0:
+
+            if button_count != 0 and button_count % button_columns == 0:
                 button_row += 1
             
-            buttons[-1].grid(row=b_row, column=i % button_columns)
+            buttons[-1].grid(row=button_row, column=button_count % button_columns)
+
+            button_count += 1
             
-        button_notebook.add(sections[j],text=section["title"])
+        button_notebook.add(sections[-1], text=section["title"])
 
     parms = sorted(list(set(parms)))
 
@@ -112,7 +119,7 @@ def main():
     des_label_text = StringVar()
     des_text_text = StringVar()
     des_label = Label(des_frame, textvariable=des_label_text, justify=LEFT, anchor="w")
-    des_text = Message(des_frame, textvariable=des_text_text, justify=LEFT, anchor="w", width=200)
+    des_text = Message(des_frame, textvariable=des_text_text, justify=LEFT, anchor="w", width=300)
     des_label.grid(sticky = W, row=0,column=0)
     des_text.grid(sticky = W, row=1,column=0)
 
